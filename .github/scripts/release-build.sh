9254da257a1255ef93f6b3e46b8ef9cf4badc207
Ch.-David Blot <charles-david.blot@outscale.com>
2026-01-29 11:39:08 +0100

    🚧 draft(ci): cleanup release scripts

--- .github/scripts/release-build.sh
+++ .github/scripts/release-build.sh
@@ -2,10 +2,7 @@
 set -e
 
 root=$(cd "$(dirname $0)/../.." && pwd)
-if [ -e "$root/.auto-release-abort" ]; then
-    echo "previous step triggered stop, abort"
-    exit 0
-fi
+
 # build new version number
 local_sdk_version=$(cat $root/sdk_version)
 local_sdk_version_major=$(echo $local_sdk_version | cut -d '.' -f 1)
@@ -13,30 +10,8 @@ local_sdk_version_minor=$(echo $local_sdk_version | cut -d '.' -f 2)
 new_sdk_version_minor=$(( local_sdk_version_minor + 1 ))
 new_sdk_version="$local_sdk_version_major.$new_sdk_version_minor.0"
 
-branch_name="autobuild-$new_sdk_version"
-git branch -m $branch_name
-
 echo "$new_sdk_version" > $root/sdk_version
-
-# build release notes
-new_api_version=$(cat $root/api_version)
-release_notes="# $new_sdk_version
-
- - SDK update for Outscale API $new_api_version
-
-"
-echo "$release_notes$(cat $root/changelog.md)" > $root/changelog.md
 
 # generate SDK
 cd "$root"
 make gen
-
-# setup git && commit
-git config user.name "Outscale Bot"
-git config user.email "opensource+bot@outscale.com"
-git config gpg.format "ssh"
-git config commit.gpgSign true
-for f in docs src; do
-    git add $f || true
-done
-git commit -asm "🔖 release: osc-sdk-rust v$new_sdk_version"
